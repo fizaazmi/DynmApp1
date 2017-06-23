6fd9b84a5f4b46589b615e218a9aca80df6c46f1
fizaazmi <fiza.azmi@toppr.com>
2017-06-23 16:33:00 +0530

    API connection working

--- MainActivity.java
+++ MainActivity.java
@@ -1,67 +1,85 @@
 package com.example.hp.dynmapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
-import java.util.List;
-import android.support.v7.app.ActionBarActivity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.WindowManager;
+import java.util.Arrays;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView.LayoutManager layoutManager;
-    private List<String> listData = new ArrayList<>();
-
-
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FragmentManager fm = getFragmentManager();
-//        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-//        Fragment1 f1 = new Fragment1();
-//        fragmentTransaction.replace(android.R.id.content, f1);
-//        fragmentTransaction.commit();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        NewFragment f1 = new NewFragment();
+        fragmentTransaction.replace(android.R.id.content, f1).add(R.id.container,NewFragment.newInstance("",""));
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
 
-        setupList();
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        RecyclerAdapter adapter=new RecyclerAdapter(listData,this);
-        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    public void onFragmentInteraction(String fragmentName) {
+        if (fragmentName.equalsIgnoreCase("fragment2")) {
 
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            NewFragment2 f2 = new NewFragment2();
+            fragmentTransaction.replace(android.R.id.content, f2).replace(R.id.container,NewFragment2.newInstance("",""));
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container, NewFragment2.newInstance("", "")).commit();
+
+        }
+
+
+            else if (fragmentName.equalsIgnoreCase("fragment4")) {
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            NewFragment4 f4 = new NewFragment4();
+            fragmentTransaction.replace(android.R.id.content, f4).replace(R.id.container,NewFragment4.newInstance("",""));
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+           // getSupportFragmentManager().beginTransaction().replace(R.id.container, NewFragment4.newInstance("", "")).commit();
+
+
+
+        }
+         else
+        {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            NewFragment3 f3 = new NewFragment3();
+            fragmentTransaction.replace(android.R.id.content, f3).replace(R.id.container,NewFragment3.newInstance("",""));
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container,NewFragment3.newInstance("","")).commit();
+            //getSupportFragmentManager().popBackStack();
+        }
+        }
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
     }
 
 
-
-
-
-private void setupList()
-{
-   listData.add("Physics");
-   listData.add("Chemistry");
-    listData.add("Maths");
-    listData.add("Biology");
-    listData.add("English");
-
-
-}
-
-
-
-}
 
